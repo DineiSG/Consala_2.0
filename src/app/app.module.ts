c395fd1155ab2a3d08e6b99648fba4e7effef9cf
Dinei <waldinei.wsg@gmail.com>
2022-09-10 21:16:08 -0300

    Criação do menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaLoginComponent
+    TelaLoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
